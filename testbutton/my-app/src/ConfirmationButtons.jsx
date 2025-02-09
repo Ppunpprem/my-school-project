6f0eec38d449d3ae02181b46e6886f259176bf37
Ppunpprem <151167611+Ppunpprem@users.noreply.github.com>
2025-02-09 11:30:50 +0700

    c

--- testbutton/my-app/src/ConfirmationButtons.jsx
+++ testbutton/my-app/src/ConfirmationButtons.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import "./ConfirmationButtons.css"; // Make sure you have this CSS file
 
 const ConfirmationButtons = () => {
+  const [selected, setSelected] = useState(null);
+
   return (
     <div className="container">
-      <p className="question">Is this information correct?</p>
       <div className="button-group">
-        <button className="yes-button">Yes</button>
-        <button className="no-button">No?</button>
+        <button
+          className={selected === "yes" ? "yes-button selected" : "yes-button"}
+          onClick={() => setSelected("yes")}
+        >
+          Yes
+        </button>
+        <button
+          className={selected === "no" ? "no-button selected" : "no-button"}
+          onClick={() => setSelected("no")}
+        >
+          No?
+        </button>
       </div>
     </div>
   );
